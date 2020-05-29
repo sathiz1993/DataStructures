87c3daa9d7423c863fd301909180944169cc753e
Sathish Kumar <sathiz1993@gmail.com>
2020-05-29 20:43:57 +0530

    Initial commit

---
+++ python/linkedlist.py
@@ -0,0 +1,17 @@
+class Node:
+    """
+    Node instance
+    """
+
+    def __init__(self, data):
+        self.data = data  # Assign data
+        self.next = None
+
+
+class LinkedList:
+    """
+    Linkeded list instance
+    """
+
+    def __init__(self):
+        self.head = None
